b3ea2a343074ba2aefcf92a228f3a1bbbe41d493
me <meh@meh>
2024-03-17 13:27:15 +0700

    make changes like these

---
+++ packages/studk-demos/app/page.tsx
@@ -0,0 +1,65 @@
+
+import * as React from "react" ;
+
+
+
+
+
+
+
+export default function App() {
+  ;
+  const c = (
+    <div>
+      <h1>
+        This is StuDK!
+      </h1>
+      <p>
+        This is StuDK!
+      </p>
+      <p>
+        This is StuDK!
+      </p>
+    </div>
+  ) ;
+  return (
+    <div style={{ display: "block flow-root", paddingBlock: "2em 3em", paddingInline: "1em 3em", minBlockSize: "45em" }} >
+        { c }
+    </div>
+  ) ;
+} ;
+
+
+
+
+import dynamicComponent from "next/dynamic";
+
+
+
+
+// import mainOtfUrl from "otf-export/otfDemo.mjs" ;
+
+// console.log({ mainOtfUrl, }) ;
+
+
+// const KIFF = (() => {
+//   ;
+
+//   const l = () => {
+//     ;
+    
+//     const {
+//       font ,
+//       fontFamilyName ,
+//       intendedFName ,
+//     } = generateMainOtf({ dts: Date.now() }) ;
+
+//   }
+// })() ;
+
+
+
+
+
+
+
