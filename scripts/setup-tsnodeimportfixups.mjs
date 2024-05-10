cf51e275792ca2d8efa94972ca9cc194549256e5
me <meh@meh>
2024-05-10 07:51:52 +0700

    make changes like these

--- scripts/setup-tsnodeimportfixups.mjs
+++ scripts/setup-tsnodeimportfixups.mjs
@@ -20,6 +20,10 @@ import { register } from "node:module";
 /* https://github.com/TypeStrong/ts-node/issues/2100#issuecomment-2039564225 */
 register(
   "ts-node/esm",
+  /**
+   * would've otherwise default to `data:`,
+   * preventing relative imports from ever working.
+   */
   pathToFileURL("./"),
 );
 
