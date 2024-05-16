1d24d3fc41dadeafcbc15ed76d8b86f8fbd1b20f
me <meh@meh>
2024-05-16 12:25:19 +0700

    after <u> (UNFINISHED!) ENHANCEMENTS [Code] [UI Engine/Infras Packages] these enhancements not yet fully inspected/reviewed </u>

---
+++ packages/studk-ui/src/meta/react-dom/hovers-positioning-sync-1.tsx
@@ -0,0 +1,162 @@
+
+
+
+
+
+
+/* 
+ * https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#unsupported-pattern-importing-server-components-into-client-components  
+ * https://stackoverflow.com/q/77592173  
+ * https://nextjs.org/docs/app/building-your-application/rendering/client-components#how-are-client-components-rendered  
+ * 
+ * "client components" can still be rendered server-side so
+ * lets expect having `"use client"` wherever possible
+ * 
+ * */
+"use client" ;
+
+
+
+
+
+
+import {
+  util,
+} from 'typexpe-commons/src/common_sv.mjs';
+
+import {
+  random,
+} from "lodash-es" ;
+
+import {
+  MNI_CTXTUALONLY ,
+  mkArray ,
+} from '#currentPkg/src/fwCore/ewo.ts'; ;
+
+import type {
+  ArgsGetOptions ,
+  ArgsWithOptions ,
+} from '#currentPkg/src/fwCore/ewo.ts'; ;
+
+import type {
+  ContinuousLinearRange ,
+} from '#currentPkg/src/fwCore/linearValues.ts'; ;
+
+const TIMEOUT = (
+  (tMillis: number) => (
+    new Promise<void>(resume => (
+      setTimeout(resume, tMillis)
+    ))
+  )
+) ;
+
+
+import {
+  Point2D ,
+} from "studk-util/src/math/point-all.mjs" ;
+
+
+
+
+
+
+import * as React from "react" ;
+
+import {
+  useIntervalEffect ,
+  useIntervalScan ,
+  useMutableRefObjState ,
+  useRefState ,
+} from "studk-ui/src/meta/react-dom/ovc-util.tsx" ;
+
+import * as ReactDOM from "react-dom" ;
+
+
+
+
+
+;
+
+import {
+  getNativeCompPosition,
+  useNativeCompPosition,
+} from "studk-ui/src/meta/react-dom/computedstyles1.tsx" ;
+
+import type {
+  //
+  NcpSupportedElem ,
+} from "studk-ui/src/meta/dom/computedstyles1.tsx" ;
+
+const useNativeCompPositionSyncRef = (
+  function (...[e, mde] : [NcpSupportedElem, NCPSR.Subject] )
+  {
+    ;
+
+    const ncpRef = React.useRef<HTMLDivElement | null>(null) ;
+
+    useIntervalEffect(() => {
+      const e1 = ncpRef.current ;
+      if (e1) {
+        const ncp = getNativeCompPosition(e) ;
+        if ((e1.hidden = !(!!ncp) , ncp) )
+        {
+          C :
+          switch (mde) {
+            //
+            case NCPSR.Subject.BOTTOM :
+            {
+              Object.assign(e1.style, {
+                //
+                top : `${`${ncp.bottomPos }px` } ` , 
+                left: `${`${ncp.pos["x"] }px` } ` , 
+              } ) ;
+              break C ;
+            }
+            case NCPSR.Subject.BOUNDINGBOX :
+            {
+              Object.assign(e1.style, {
+                //
+                top : `${`${ncp.pos.y }px` } ` , 
+                left: `${`${ncp.pos["x"] }px` } ` , 
+                height : `${`${ncp.bottomPos - ncp.pos.y }px` } ` , 
+                width : `${`${ncp.rightPos - ncp.pos.x }px` } ` , 
+              } satisfies React.CSSProperties ) ;
+              break C ;
+            }
+          }
+        }
+      }
+    } , 0.0551 * 1000 , [ncpRef] ) ;
+    ;
+
+    return ncpRef ;
+  }
+) ;
+
+namespace NCPSR { ; }
+
+namespace NCPSR
+{
+  ;
+  export enum Subject {
+    BOTTOM = 1 << 1 ,
+    BOUNDINGBOX = 1 << 3 ,
+  }
+}
+
+export {
+  useNativeCompPositionSyncRef ,
+  NCPSR ,
+} ;
+
+export type {
+  // /** @deprecated */
+  NcpSupportedElem ,
+} ;
+
+
+
+
+
+
+
---
+++ packages/studk-ui/src/meta/react/withAdHocRefs.tsx
@@ -0,0 +1,181 @@
+
+
+
+
+
+
+/* 
+ * https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#unsupported-pattern-importing-server-components-into-client-components
+ * https://stackoverflow.com/q/77592173
+ * https://nextjs.org/docs/app/building-your-application/rendering/client-components#how-are-client-components-rendered
+ * 
+ * "client components" can still be rendered server-side so
+ * lets expect having `"use client"` wherever possible
+ * 
+ * */
+"use client" ;
+
+
+
+
+
+
+import {
+  util,
+} from 'typexpe-commons/src/common_sv.mjs';
+
+import {
+  random,
+} from "lodash-es" ;
+
+import type {
+  ArgsGetOptions ,
+  ArgsWithOptions ,
+} from '#currentPkg/src/fwCore/ewo.ts'; ;
+
+
+import {
+  Point2D ,
+} from "studk-util/src/math/point-all.mjs" ;
+
+
+
+
+
+
+import * as React from "react" ;
+
+import {
+  describeComponent,
+} from 'studk-ui/src/meta/react/dec.tsx'; ;
+
+
+import * as ReactDOM from "react-dom" ;
+
+type IntrinsicElementRef< tagNameT extends (
+  keyof JSX.IntrinsicElements
+)> = (
+  Extract<(
+    (
+      JSX.IntrinsicElements[tagNameT]["ref"]
+    )
+  ), React.Ref<any> >
+) ;
+
+export type {
+  IntrinsicElementRef ,
+} ;
+
+const destructureElement = (
+  function <
+    //
+    const tagNameT extends (
+      React.JSXElementConstructor<any>
+      | string
+    ) ,
+    const propsT extends React.ReactElement<{ ref ?: React.Ref<refEdElemT>, children ?: any, }, tagNameT >["props"] ,
+    const refEdElemT = never ,
+  >(...[srcE ] : (
+    ArgsWithOptions<[original: React.ReactElement<propsT, tagNameT>, ], {} >
+  ) )
+  {
+    const {
+      key,
+      type,
+      props: {
+        ref ,
+        children ,
+        ...otherProps
+      } ,
+    } = srcE ;
+
+    return {
+      key,
+      type ,
+      ref ,
+      children ,
+      otherProps ,
+    } as const ;
+  }
+) ;
+
+const withRef: (
+  & {
+    <const tagNameT extends keyof JSX.IntrinsicElements>(...args: (
+      ArgsWithOptions<[IntrinsicElementRef<tagNameT> , original: React.ReactElement<any, tagNameT >, ], {} >
+    ) ): React.ReactElement
+
+    /** @deprecated */
+    <const tagNameT extends keyof JSX.IntrinsicElements>(...args: (
+      ArgsWithOptions<[IntrinsicElementRef<tagNameT> , original: React.ReactElement, ], {} >
+    ) ): React.ReactElement
+
+  }
+) = (
+  function withRefElemImpl<const tagNameT extends (
+    keyof JSX.IntrinsicElements
+  )>(...[intendedRef, srcE ] : (
+    ArgsWithOptions<[IntrinsicElementRef<tagNameT> , original: React.ReactElement, ], {} >
+  ) )
+  : React.ReactElement
+  {
+    if (typeof srcE.type === "string")
+    {
+      const {
+        key,
+        ref: existingRef,
+        type ,
+        children,
+        otherProps ,
+      } = (
+        destructureElement(srcE)
+      ) ;
+      return (
+        React.createElement(type, {
+          key,
+          ref: existingRef ?? intendedRef,
+          ...otherProps,
+        }, children )
+      ) ;
+    }
+
+    if (srcE.type === React.Fragment)
+    {
+      // TODO
+    }
+
+    // {
+    //   const s = React.Children.toArray(srcE) ;
+    //   if (s.length === 1) {
+    //     return l
+    //   }
+    // }
+
+    return srcE ;
+  }
+) ;
+
+/**
+ * test the strict sig check
+ * 
+ */
+{ (...[ref, e]: [React.Ref<HTMLDivElement>, React.ReactElement ] ) => {
+  ;
+  withRef(ref, e) ;
+  withRef(ref, e as React.ReactElement<any, "div">) ;
+} }
+
+export {
+  withRef ,
+} ;
+
+
+
+
+
+
+
+
+
+
+
