307b014e1627f80f07e5e3b9abc7a94e6b387b74
me <meh@meh>
2024-05-28 01:22:15 +0700

    make changes like these

---
+++ packages/studk-ui-demos/components/spcl/spHitsMeAppDiscogrDemo.tsx
@@ -0,0 +1,153 @@
+
+
+
+
+
+
+
+
+
+
+/* 
+ * https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#unsupported-pattern-importing-server-components-into-client-components  
+ * https://stackoverflow.com/q/77592173  
+ * https://nextjs.org/docs/app/building-your-application/rendering/client-components#how-are-client-components-rendered  
+ * 
+ * "client components" can still be rendered server-side so
+ * lets expect having `"use client"` wherever possible
+ * 
+ * */
+"use client" ;
+
+
+
+
+
+
+import {
+  util,
+} from 'typexpe-commons/src/common_sv.mjs';
+
+import {
+  random,
+} from "lodash-es" ;
+
+import type {
+  ArgsGetOptions ,
+  ArgsWithOptions ,
+} from 'studk-fwcore-setups/src/util-eawo.mjs'; ;
+
+
+
+
+
+
+;
+
+import * as React from "react" ;
+
+
+
+
+
+
+import {
+  pagesConventions,
+  dynamicComponent,
+  AppLink,
+  Image,
+} from "@/appInternalScripts/appPagesConvention"; ;
+
+import {
+  describeHeadlinedArticle ,
+  describeWorksheet ,
+} from 'studk-ui/src/meta/react/dhc.tsx'; ;
+
+import {
+  Span ,
+  Button ,
+} from 'studk-ui/src/meta/react/dbc.tsx'; ;
+
+;
+
+
+
+
+;
+
+import {
+  MspStackingC ,
+  MspPhrasalBlockC ,
+  MspPhrasalTextColC ,
+  BackgroundedAdvBlockC ,
+  PicturingAdvBlockC ,
+  describeUrlIcoApplet, 
+  describeSvgApplet,
+} from 'studk-ui/src/xst/prefabs/summerhitsmedia-textblocks-b.tsx'; ;
+
+import {
+  BlockLvlSvgC ,
+  InlineLvlSvgC ,
+} from 'studk-ui/src/xst/svgd.tsx'; ;
+
+import {
+  describeCallbackAssignedStyleProps ,
+} from 'studk-ui/src/xst/prefabs/summerhitsmedia-cssd.tsx'; ;
+
+import {
+  getGsSharpenFltUrl,
+  getSvgFilterAsCssFilterUrlFast ,
+} from 'studk-ui/src/xst/cssFilterUrl.tsx'; ;
+
+
+export const DiscogrDemoC = (
+  function DiscogrDemoCImpl() {
+    return (
+      describeHeadlinedArticle({
+        heading: (
+          <span>
+            Discography
+          </span>
+        ) ,
+        children: (
+          <div>
+            <p>Discography Really Matters</p>
+            <p>Discography Really Matters</p>
+
+            <figure>
+              <BackgroundedAdvBlockC
+              ico={(
+                <div />
+              )}
+              children={(
+                <MspPhrasalBlockC>
+                  <p>
+                    Phrasal
+                  </p>
+                </MspPhrasalBlockC>
+              )}
+              />
+            </figure>
+            
+            { SPHMEA.SUMMERDISCO.renderedFigure }
+
+            { SPHMEA.CHILLING_EUR_BIENVENUE.renderedFigure }
+
+            { SPHMEA.CHILLING_EUR_BIENVENUE.renderedFigure }
+
+          </div>
+        ) ,
+      })
+    ) ;
+  }
+) ;
+
+import {
+  SPHMEA ,
+} from "./spHitsMeAppDiscogrDemoItems.tsx" ;
+
+import vbvImgUrl from "studk-ui-encore/src/venicebienvenue.png" ;
+
+
+
+
---
+++ packages/studk-ui/src/xst/prefabs/summerhitsmedia-cssd.tsx
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+/* 
+ * https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#unsupported-pattern-importing-server-components-into-client-components  
+ * https://stackoverflow.com/q/77592173  
+ * https://nextjs.org/docs/app/building-your-application/rendering/client-components#how-are-client-components-rendered  
+ * 
+ * "client components" can still be rendered server-side so
+ * lets expect having `"use client"` wherever possible
+ * 
+ * */
+"use client" ;
+
+
+
+
+
+
+import {
+  util,
+} from 'typexpe-commons/src/common_sv.mjs';
+
+import {
+  random,
+} from "lodash-es" ;
+
+import {
+  MNI_CTXTUALONLY ,
+  mkArray ,
+} from '#currentPkg/src/fwCore/ewo.ts'; ;
+
+import type {
+  ArgsGetOptions ,
+  ArgsWithOptions, 
+  Extend,
+} from 'studk-fwcore-setups/src/util-eawo.mjs'; ;
+
+import type {
+  // ContinuousLinearRange ,
+} from '#currentPkg/src/fwCore/linearValues.ts'; ;
+
+
+
+
+
+
+import * as React from "react" ;
+
+
+
+
+
+;
+
+type MutableCSSProperties = (
+  React.CSSProperties extends infer T ? (
+    { [k in Exclude<keyof T, never>] : T[k] ; }
+  ) : never
+) ;
+
+export const describeCallbackAssignedStyleProps = (
+  function (...[init] : [(self: MutableCSSProperties) => void ])
+  : React.CSSProperties
+  {
+    const e = new Object() as MutableCSSProperties ;
+    init(e) ;
+    return (
+      /* object possibly leaked somewhere else. */
+      { ...e } as const
+    ) ;
+  }
+) ;
+
+export const describeByCssStringStyleProps = (
+  function (...[c] : [HTMLStyleElement["innerHTML"] ])
+  : React.CSSProperties
+  {
+    const e = (
+      document.createElement("div") as (
+        Extend<Pick<HTMLElement, "setAttribute">, { style: MutableCSSProperties, } >
+      )
+    ) ;
+    e.setAttribute("style", c) ;
+    return (
+      /* object possibly leaked somewhere else. */
+      { ...(e.style) } as const
+    ) ;
+  }
+) ;
+
+
+
+
+
+
+
+
