effc32ff9b5415025c3c5785f7c25b405bb4bd45
me <meh@meh>
2024-05-30 14:37:30 +0700

    make changes like these

--- packages/studk-ui/src/meta/react/withAdHocRefs.tsx
+++ packages/studk-ui/src/meta/react/withAdHocRefs.tsx
@@ -50,7 +50,7 @@ import {
 } from 'studk-ui/src/meta/react/dec.tsx'; ;
 
 
-import * as ReactDOM from "react-dom" ;
+import * as ReactDOM from "studk-fbreact-all/src/react-dom-min-1.ts" ;
 
 type IntrinsicElementRef< tagNameT extends (
   keyof JSX.IntrinsicElements
@@ -76,7 +76,10 @@ const destructureElement = (
     const propsT extends (
       React.ReactElement<{ ref ?: (
         tagNameT extends (keyof JSX.IntrinsicElements) ?
-        React.ElementRef<tagNameT>
+        React.Ref<(
+          /** naming {@link React.ElementRef} doesn't agree */
+          React.ElementRef<tagNameT>
+        ) >
         : React.Ref<refEdElemT>
       ), children ?: any, }, tagNameT >["props"]
     ) ,
@@ -110,10 +113,20 @@ const withRef: (
     <const tagNameT extends keyof JSX.IntrinsicElements>(...args: (
       ArgsWithOptions<[IntrinsicElementRef<tagNameT> , original: React.ReactElement<any, tagNameT >, ], {} >
     ) ): React.ReactElement
+    
+    /** widened */
+    <const tagNameT extends keyof JSX.IntrinsicElements>(...args: (
+      ArgsWithOptions<[React.Ref<React.ElementRef<tagNameT> > , original: React.ReactElement<any, tagNameT >, ], {} >
+    ) ): React.ReactElement
 
     /** @deprecated */
     <const tagNameT extends keyof JSX.IntrinsicElements>(...args: (
       ArgsWithOptions<[IntrinsicElementRef<tagNameT> , original: React.ReactElement, ], {} >
+    ) ): React.ReactElement
+    
+    /** @deprecated */
+    <const tagNameT extends keyof JSX.IntrinsicElements>(...args: (
+      ArgsWithOptions<[React.Ref<React.ElementRef<tagNameT> > , original: React.ReactElement, ], {} >
     ) ): React.ReactElement
 
   }
@@ -139,7 +152,7 @@ const withRef: (
       return (
         React.createElement(type, {
           key,
-          ref: combineRefs(intendedRef, existingRef),
+          ref: combineRefs(intendedRef ?? null, existingRef ?? null),
           ...otherProps,
         }, children )
       ) ;
@@ -162,21 +175,21 @@ const withRef: (
 ) ;
 
 const combineRefs: {
-  <tagNameT extends keyof JSX.IntrinsicElements>(...[intendedRef, existingRef] : (
+  <tagNameT extends keyof JSX.IntrinsicElements>(...[intendedRef, existingRef] : [...(
     ArgsWithOptions<(
-      | [...[ IntrinsicElementRef<tagNameT> ] , original?: IntrinsicElementRef<tagNameT>, ]
-      | [...[ IntrinsicElementRef<tagNameT>? ] , original: IntrinsicElementRef<tagNameT>, ]
+      | [...[ IntrinsicElementRef<tagNameT> ] , original: IntrinsicElementRef<tagNameT> | null, ]
+      | [...[ IntrinsicElementRef<tagNameT> | null ] , original: IntrinsicElementRef<tagNameT>, ]
     ), {} >
-  )): IntrinsicElementRef<tagNameT> ;
+  )]): IntrinsicElementRef<tagNameT> ;
 
-  <tagNameT extends keyof JSX.IntrinsicElements>(...[intendedRef, existingRef] : (
-    ArgsWithOptions<[...[ IntrinsicElementRef<tagNameT> ? ] , original?: IntrinsicElementRef<tagNameT>, ], {} >
-  )): IntrinsicElementRef<tagNameT> | undefined ;
+  <tagNameT extends keyof JSX.IntrinsicElements>(...[intendedRef, existingRef] : [...(
+    ArgsWithOptions<[...[ IntrinsicElementRef<tagNameT> | null ] , original: IntrinsicElementRef<tagNameT> | null, ], {} >
+  )]): IntrinsicElementRef<tagNameT> | undefined ;
 
 } = (
   // ArgsWithOptions<[IntrinsicElementRef<tagNameT> , original: React.ReactElement<any, tagNameT>, ], {} >
   function <tagNameT extends keyof JSX.IntrinsicElements>(...[intendedRef, existingRef] : (
-    ArgsWithOptions<[...[ IntrinsicElementRef<tagNameT> ? ] , original?: IntrinsicElementRef<tagNameT>, ], {} >
+    ArgsWithOptions<[...[ IntrinsicElementRef<tagNameT> | null ] , original: IntrinsicElementRef<tagNameT> | null, ], {} >
   ))
   {
     return (
