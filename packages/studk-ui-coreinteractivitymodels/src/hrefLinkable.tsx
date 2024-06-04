d232063988a717f6d4a25db7c382a9894ece6f9f
me <meh@meh>
2024-06-04 14:51:56 +0700

    after [packages] [StuDK UI] make changes like these (primarily refactoring)

---
+++ packages/studk-ui-coreinteractivitymodels/src/hrefLinkable.tsx
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+;
+
+
+
+
+
+import {
+  util,
+} from 'typexpe-commons/src/common_sv.mjs';
+
+
+
+
+
+
+export type FullDocHref = `${Location["origin"]}${Location["pathname"]}` ;
+
+export type BrowsibleHref = Location["href"] ;
+
+export type LinkableHref = HTMLAnchorElement["href"] & {} ;
+
+
+
+
+
+
+
+
+
