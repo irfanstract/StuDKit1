ac2b12aa0ef122af8a4f2f4704525061c932ad97
me <meh@meh>
2024-07-31 18:36:48 +0700

    tree after (UNFINISHED!) CHANGES [packages] [*StuDK Internal FW Core For UI*] *amend `ComponentProps` and `RequiredComponentProps` this way;  ;*

--- packages/studk-ui-fwcore/src/util/ReactJsBased.ts
+++ packages/studk-ui-fwcore/src/util/ReactJsBased.ts
@@ -111,19 +111,33 @@ export const getFromClassNameProp = (
 ;
 
 export type {
-  ComponentProps ,
+  // ComponentProps ,
+  /** @deprecated */
   ComponentPropsWithoutRef ,
 } from "react" ;
 
 /**
- * applies {@link React.ComponentProps `React.ComponentProps<T>`} and then {@link Required `Required`}.
+ * applies {@link ComponentProps `React.ComponentProps<T>`} and then {@link Required `Required`}.
  * 
  */
 export type RequiredComponentProps<T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>> = (
   Required<(
-    React.ComponentProps<T>
+    ComponentProps<T>
   )>
 ) ;
+
+export type ComponentProps<T extends React.JSXElementConstructor<any> | keyof React.JSX.IntrinsicElements> = (
+
+  [T] extends [React.JSXElementConstructor<infer P>] ?
+  P
+  :
+
+  [T] extends [keyof React.JSX.IntrinsicElements] ?
+  React.JSX.IntrinsicElements[T]
+  :
+
+  {}
+) ; // ComponentProps
 
 import {
   describeComponent,
