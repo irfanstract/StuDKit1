7302d5ba560fb054db6a5ce66dd13f1574ed01ec
me <irfankf@gmail.com>
2024-08-04 20:07:20 +0700

    tree after UNFINISHED CHANGES [packages] [*Local FW For UI*] [*React/JSX Helpers*] *do these rework;  ;*

--- packages/studk-ui-fwcore/src/reactjs/helpers/UseReactSetStateAction.tsx
+++ packages/studk-ui-fwcore/src/reactjs/helpers/UseReactSetStateAction.tsx
@@ -45,13 +45,26 @@ import type * as React from "react" ;
 namespace ReactSetStateActionHelpers {
   ;
 
-  export function asDigestFnc<S , SF extends (
-    // (S extends ((...args: any) => any ) ? never : (React.SetStateAction<S> ) )
-    React.SetStateAction<S>
-  )>(f0: SF) : ((x0: S) => S)
+  export function asDigestFnc<S , >(f0: React.SetStateAction<S>)
+  : ((x0: S) => S)
+  {
+    return (
+      asDigestFnc1(f0)
+    ) ;
+  }
+
+  export function asDigestFnc1<SA, const S0 extends SA , const S2 extends SA >(f0: S2 | ((x0: SA ) => S2 ))
+  : ((x0: S0 | SA ) => S2)
   {
     return (
       (typeof f0 === "function") ? f0 : (() => f0 )
+    ) ;
+  }
+
+  export function runSpcl<SA, S0 extends SA , S2 extends SA >(...[f0, v0] : [S2 | ((x0: SA ) => S2 ), prevValue: S0 & SA ] )
+  {
+    return (
+      (asDigestFnc1(f0) )(v0)
     ) ;
   }
 
