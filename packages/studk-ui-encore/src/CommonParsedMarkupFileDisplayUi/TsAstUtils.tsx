6cbca82962d6cdcf3ad5de3ea144777fd134047b
me <irfankf@gmail.com>
2024-08-14 06:33:34 +0700

    tree **after** *[Packages] [Data Structures Transforms And UI Infras] do these refactoring-and-reorg, (`CommonParsedMarkupFileDisplayUi`'s) `TsAstUtils.tsx` and minutely `TsAstDisplay.tsx`, splitting into multiple different files (yielding, in `studk-ts-codeanalysis/src`, `TsAstInspectiveUtil.ts` and `TsAstScaffoldedExample.ts`), possibly with some minor chgs*
    ```
    Changes to be committed:
    new file:   packages/studk-ts-codeanalysis/src/core/TsAstInspectiveUtil.ts
    new file:   packages/studk-ts-codeanalysis/src/core/TsAstScaffoldedExample.ts
    modified:   packages/studk-ui-encore/src/CommonParsedMarkupFileDisplayUi/TsAstDisplay.tsx
    modified:   packages/studk-ui-encore/src/CommonParsedMarkupFileDisplayUi/TsAstUtils.tsx
    ```
    
    *[Packages] [Data Structures Transforms And UI Infras]*

--- packages/studk-ui-encore/src/CommonParsedMarkupFileDisplayUi/TsAstUtils.tsx
+++ packages/studk-ui-encore/src/CommonParsedMarkupFileDisplayUi/TsAstUtils.tsx
@@ -37,66 +37,32 @@ import { TS, } from "studk-fwcore/src/scripting/TsLib.ts" ;
 
 
 
+import {
+  getNodeTypeLabelTxt ,
+  getNodeChildren ,
+} from "studk-ts-codeanalysis/src/core/TsAstInspectiveUtil.ts" ;
+
+export {
+  getNodeTypeLabelTxt ,
+} ;
 
 
 
 
 
 
-export const getSampleTsSourceFile = (
-  function ()
-  {
-    const sf = (
-      TS.createSourceFile("<repl>", (
-        `
-        import {
-          CMath ,
-          LinearAlgebra ,
-          ElementaryCalculus ,
-        } from "node:calculus" ;
-        import {
-          describeXyzLocation ,
-        } from "node:three_dimensional" ;
 
-        const x = 1 ;
-        const z = 1.5 ;
-        const loc = (
-          <point x={x} y={0} z={z} />
-        ) ;
+import {
+  getSampleTsSourceFile ,
+} from "studk-ts-codeanalysis/src/core/TsAstScaffoldedExample.ts" ;
 
-        const d = <diameter>{ 1 }</diameter> ;
+export {
+  getSampleTsSourceFile ,
+} ;
 
-        export default (
-          <cube origin={loc} diameter={d} />
-        ) ;
-
-        {
-          "unused string literal" ;
-          if (1) {
-            "unused string literal" ;
-          }
-
-          { 5; 6; 7; }
-          [5, 6, 7] ;
-          
-          [5, 6, 7] as const ;
-        }
-
-        `
-      ) , {
-        languageVersion: TS.ScriptTarget.ES2022,
-      }, (
-        /**
-         * things break when you set this to `false`.
-         * we shall always set this to `true`.
-         * 
-         */
-        true
-      ), TS.ScriptKind.TSX )
-    ) ;
-    return sf ;
-  }
-) ;
+export {
+  getNodeChildren ,
+} ;
 
 
 
