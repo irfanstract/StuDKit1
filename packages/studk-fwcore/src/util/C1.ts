282b6548700d9df6f4391972e7aeb9d579cbe8d7
me <irfankf@gmail.com>
2024-08-16 18:13:53 +0700

    after <u>[General-Purpose Utility-Funcs Pkg(s) And Internal FW] (utility type-def(s)) scaffold `C1.ts` this way</u>

---
+++ packages/studk-fwcore/src/util/C1.ts
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {
+  util,
+} from 'typexpe-commons/src/common_sv.mjs';
+
+
+export type * from "studk-util/src/utilityTypeDefs/ArgsWithOptions.mjs" ;
+
+
+export type * from "studk-util/src/utilityTypeDefs/OITF.mjs" ;
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
