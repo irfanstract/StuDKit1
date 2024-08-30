4753813a7fe79541e68409457310b9e8fd9592c9
me <irfankf@gmail.com>
2024-08-31 00:55:06 +0700

    after <u> (UNFINISHED!) ENHANCEMENTS [Code] [UI Engine/Infras Packages] these enhancements, _possibly_ involving `package.json` or `tsconfig`, not yet fully inspected/reviewed </u>

---
+++ packages/studk-dom-util/src/SvgDocUrlFmt1.tsx
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+/* 
+ * https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#unsupported-pattern-importing-server-components-into-client-components  
+ * https://stackoverflow.com/q/77592173  
+ * https://nextjs.org/docs/app/building-your-application/rendering/client-components#how-are-client-components-rendered  
+ * 
+ * "client components" can still be rendered server-side so
+ * lets expect having `"use client"` wherever possible
+ * 
+ * */
+"use client" ;
+
+
+
+
+
+
+import {
+  util,
+} from 'typexpe-commons/src/common_sv.mjs';
+
+import type {
+  ArgsGetOptions ,
+  ArgsWithOptions, 
+  Extend,
+} from 'studk-fwcore-setups/src/util-eawo.mjs'; ;
+
+
+
+
+
+
+const getFullDocSrcBasedSvgDataUrl = (
+  function (...[{} = {}, d]: [...ArgsWithOptions<[], {}>, src: string]) {
+    return (
+      (
+        "data:image/svg+xml," + encodeURIComponent(d)
+      )
+    ) ;
+  }
+) ;
+
+/**
+ * the `innerHTML`-only-based version of {@link getFullDocSrcBasedSvgDataUrl}
+ * 
+ */
+const getFullDocBodySrcBasedSvgDataUrl = (
+  function (...[{ viewBox: viewBoxSpec, }, contents]: [...ArgsWithOptions<[], { viewBox: string, }>, src: string]) {
+    return (
+      (
+        getFullDocSrcBasedSvgDataUrl({}, `
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="${viewBoxSpec }"
+        >
+          ${contents }
+        `)
+      )
+    ) ;
+  }
+) ;
+
+export {
+  getFullDocBodySrcBasedSvgDataUrl ,
+  getFullDocSrcBasedSvgDataUrl ,
+  /** @deprecated alias of {@link getFullDocSrcBasedSvgDataUrl} */
+  getFullDocSrcBasedSvgDataUrl as getSvgDataUrl,
+} ;
+
+;
+
+
+
+
+;
+
+
+
+
+
+
+
+
+
+
+
