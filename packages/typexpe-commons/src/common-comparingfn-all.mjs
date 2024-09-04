9f11005f62c03ff8c24c1cec61dc04a53431df17
me <irfankf@gmail.com>
2024-09-04 13:05:35 +0700

    after <u> (UNFINISHED!) ENHANCEMENTS [Code] [`packages/typexpe-commons` and `scripts`] these enhancements, to `common-comparingfn-all.mjs` and `scripts`, not yet fully inspected/reviewed </u>

--- packages/typexpe-commons/src/common-comparingfn-all.mjs
+++ packages/typexpe-commons/src/common-comparingfn-all.mjs
@@ -15,6 +15,12 @@ import "core-js/modules/es.array.to-sorted.js" ;
 
 
 /**
+ * @typedef {SpecificComparator<any> }
+ */
+/** @module */
+const Comparator = {} ;
+
+/**
  * @typedef {<const E>(a: E, b: E) => number }
  * 
  * @deprecated
@@ -23,16 +29,27 @@ import "core-js/modules/es.array.to-sorted.js" ;
 const GenericComparator = {} ;
 
 /**
+ * @typedef {SpecificComparator<unknown> }
+ */
+/** @module */
+const PlainComparator = {} ;
+
+/**
  * @typedef {(a: E, b: E) => number }
  * @template E
  */
 /** @module */
 const SpecificComparator = {} ;
 
+/**
+ * a {@link Comparator}
+ * to behave equivalent to {@link Array.prototype.toSorted}'s default
+ * 
+ */
 const defaultCompare = (
   /**
    * 
-   * @type {SpecificComparator<any> }
+   * @type {PlainComparator }
    */
   (...[a, b]) => {
     const r = [a, b].toSorted() ;
@@ -42,13 +59,20 @@ const defaultCompare = (
 
 /**
  * 
+ * arithmetic {@link Comparator}
+ * 
  * @type {SpecificComparator<number> | SpecificComparator<bigint> }
+ * 
  */
 const numericCompare = (
   // @ts-ignore
   (...[a, b]) => (a - b)
 ) ;
 
+/**
+ * 
+ * 
+ */
 const getReverseComparator = (
   /**
    * 
@@ -59,13 +83,42 @@ const getReverseComparator = (
   )
 ) ;
 
+/**
+ * 
+ * [utility] a {@link Comparator} to first run `_1(item)` and then compare the return-value(s) as usual
+ * 
+ */
 const getKeyingComparator = (
   /**
    * 
-   * @type {<SrcE, E>(...a: [(x: SrcE) => E, SpecificComparator<E>]) => SpecificComparator<SrcE> }
+   * a {@link Comparator} to first run `_1(item)` and then compare the return-value(s) as usual
+   * 
+   * @type {<SrcE, E>(...a: [_1: (x: SrcE) => E, _2: SpecificComparator<E>]) => SpecificComparator<SrcE> }
+   * 
    */
   (...[emap, c]) => (
     (...[a0, a1]) => c(...[emap(a0), emap(a1) ])
+  )
+) ;
+
+/**
+ * 
+ * 
+ */
+const fromLessthanPredicate = (
+  /**
+   * 
+   * @type {<SrcE,>(...a: [(...x: [SrcE, SrcE]) => boolean, ]) => SpecificComparator<SrcE> }
+   */
+  (...[isLt, ]) => (
+    (...[a0, a1]) => {
+      const wasLt = isLt(a0, a1) ;
+      const wasGt = isLt(a1, a0) ;
+      if (wasLt && wasGt ) { throw new TypeError(`inconsistent 'fromLessthanPredicate' callback resulting in both 'wasGt' and 'wasLt' at once`) ; }
+      if (wasLt ) { return -1 ; }
+      if (wasGt ) { return 1 ; }
+      return 0 ;
+    }
   )
 ) ;
 
@@ -73,12 +126,15 @@ const getKeyingComparator = (
 export {
   numericCompare,
   defaultCompare,
+  fromLessthanPredicate,
   getReverseComparator,
   getKeyingComparator,
 } ;
 
 export {
+  Comparator,
   GenericComparator,
+  PlainComparator,
   SpecificComparator ,
 } ;
 
