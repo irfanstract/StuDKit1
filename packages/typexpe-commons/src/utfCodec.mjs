9e516593352080c06798c0bd4b4dfba95bec2716
me <irfankf@gmail.com>
2024-09-18 08:11:29 +0700

    after <u> (UNFINISHED!) ENHANCEMENTS [Code] [DOM `Blob`/`File` API ] these enhancements not yet fully inspected/reviewed </u>

--- packages/typexpe-commons/src/utfCodec.mjs
+++ packages/typexpe-commons/src/utfCodec.mjs
@@ -10,6 +10,13 @@
 
 
 
+export const blobFromUtf = /** @type {(x: String, options?: { contentType ?: NonNullable<Blob["type"]> , } ) => Blob } */ (...[x, { contentType = "text/plain" } = {} ] ) => new Blob([x], { type: contentType, } ) ;
+
+
+
+
+
+
 
 
 
