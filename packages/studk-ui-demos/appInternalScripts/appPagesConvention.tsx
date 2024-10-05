13c0cb69eb74bc1b05393adcc65d2bc46ee6a899
me <irfankf@gmail.com>
2024-10-05 19:48:23 +0700

    after <u> (UNFINISHED!) ENHANCEMENTS [Packages] [Demo App Package(s) ] these refactoring (part 2 of 2 - contractionating those, into just being `import`ed ) </u>

--- packages/studk-ui-demos/appInternalScripts/appPagesConvention.tsx
+++ packages/studk-ui-demos/appInternalScripts/appPagesConvention.tsx
@@ -38,196 +38,47 @@ export {
 
 
 import {
+
+  usePathname,
+  useSearchParams ,
+  useSearchParamState ,
+  useSearchParamDictItemState ,
+  useRouter,
+  getQueryStringFromProps,
+
+  AppLink,
+  NavigateBackButton,
+  NavigateForwardButton ,
+
   React ,
-  getSpaceSeparatedClassNameList, 
-  StudkReactJs,
-  ReactSetStateActionHelpers,
-} from 'studk-ui-fwcore/src/util/ReactJsBased'; ;
+  getSpaceSeparatedClassNameList ,
+  StudkReactJs ,
+  ReactSetStateActionHelpers ,
+  dynamicComponent ,
+  SingleChildDiv ,
+  MainAndNavAndFinaleC ,
+
+  NxImage ,
+
+} from "studk-ui-fwcore/src/util/NextJsSpecificRoutedGoodies1" ;
 
 export {
   React ,
   getSpaceSeparatedClassNameList, 
   StudkReactJs,
 } ;
-
-// import dynamicComponent from "next/dynamic";
-const dynamicComponent: (
-  // | (typeof util.L.identity)
-  | (typeof import("next/dynamic").default )
-  | null
-) = (
-  null
-) ;
 
 export {
   dynamicComponent as dynamicComponent,
 } ;
 
-import {
-  SingleChildDiv,
-} from "studk-ui/src/xst/prefabs/studkdem-esingulardiv.tsx"; ;
-
 export {
   SingleChildDiv ,
 } ;
 
-import { MainAndNavAndFinaleC, } from "studk-ui/src/xst/prefabs/studkdem-semanticlayout-site-mnavf.tsx" ;
-
 export {
   MainAndNavAndFinaleC ,
 } ;
-
-import {
-  useSearchParams ,
-  usePathname,
-} from "next/navigation";
-
-import AppLink from "next/link";
-
-const useSearchParamState = (
-
-  function () {
-
-    const pathname = (
-      usePathname()
-    ) ;
-    /**
-     * {@link useSearchParams}.
-     * 
-     * due to the usage of {@link React.useMemo},
-     * to avoid rerender-loop
-     * we convert them to `String` first
-     * 
-     */
-    const queryString = (
-      useSearchParams().toString()
-    ) ;
-
-    const router = useRouter() ;
-
-    return (
-
-      React.useMemo(() => {
-
-        const fmatAfterSearchParamReplace = (
-
-          (v1: ReadonlyURLSearchParams | string): string => (
-            [pathname, "?", v1.toString() ]
-            .join("")
-          )
-        ) ;
-
-        const pushSearchParamsState = (
-
-          (...a : (
-            ArgsWithOptions<[newValue: ReadonlyURLSearchParams | string] , {
-              overwrite ?: boolean ,
-            }>
-          ) ) => {
-
-            const [
-              v1,
-              optsArg,
-            ] = a ;
-            const {
-              overwrite = false ,
-            } = optsArg ?? {} ;
-
-            return (
-              router[overwrite ? "replace" : "push"]((
-                fmatAfterSearchParamReplace(v1)
-              ) )
-            ) ;
-          }
-        ) ;
-    
-        return (
-          [queryString, {
-    
-            queryString ,
-            queryStringStructure: new ReadonlyURLSearchParams(queryString) ,
-            pathname ,
-    
-            underlyingRouter: router ,
-            pushSearchParamsState ,
-            replaceSearchParamsState: (
-              (v1: ReadonlyURLSearchParams | string) => (
-                pushSearchParamsState(v1, {
-                  overwrite: true ,
-                })
-              )
-            ) ,
-    
-          } ] as const
-        ) ;
-      } , [
-        pathname ,
-        queryString ,
-        router ,
-      ])
-    ) ;
-  }
-) ;
-
-const useSearchParamDictItemState = (
-
-  function (...[MAIN_ENTERED_QUERY] : [key: string] )
-  {
-
-    const [s, setS] = (
-
-      useSearchParamState()
-    ) ;
-
-    const sv = (
-      new ReadonlyURLSearchParams(s).get(MAIN_ENTERED_QUERY) ?? ""
-    ) ;
-
-    const setSValue = (
-
-      React.useCallback((...a : (
-        ArgsWithOptions<[newv: React.SetStateAction<string>] , {
-          //
-          overwrite ?: boolean ,
-        }>
-      )) => {
-
-        const [newv0, optsARg] = a ;
-        const {
-          //
-          overwrite: ovwArg ,
-        } = optsARg ?? {} ;
-
-        const newv = (
-
-          ReactSetStateActionHelpers.asDigestFnc(newv0 )(sv)
-        ) ;
-
-        return (
-
-          setS.pushSearchParamsState(
-            (
-              getQueryStringFromProps({
-                [MAIN_ENTERED_QUERY]: newv ,
-              })
-            ) , {
-              overwrite: ovwArg ,
-            } )
-        ) ;
-
-        ;
-      } , [
-        // TODO
-      ])
-    ) ;
-
-    return [sv, setSValue] as const ;
-  }
-) ;
-
-import {
-  useRouter ,
-} from "next/navigation";
 
 export {
   usePathname,
@@ -238,31 +89,10 @@ export {
   useRouter as useRouter, 
 } ;
 
-import {
-  NavigateBackButton,
-  NavigateForwardButton ,
-} from "studk-ui/src/meta/react/uiNavigateBackForth.tsx"; ;
-
 export {
   NavigateBackButton ,
   NavigateForwardButton ,
 } ;
-
-const getQueryStringFromProps = (
-
-  function <const actualKeyT extends string> (...[vals] : [Record<actualKeyT, string>] )
-  {
-
-    const spC = new URLSearchParams() ;
-    for (const [k, v] of Object.entries(((): Record<string, string> => vals )() ) ) {
-      spC.set(k, v) ;
-    }
-
-    return (
-      spC.toString()
-    ) ;
-  }
-) ;
 
 export {
   getQueryStringFromProps ,
@@ -273,8 +103,6 @@ export {
 // export {
 //   NextRouter ,
 // } from "next/router";
-
-import NxImage from "next/image";
 
 const Image: (
   | React.ElementType<JSX.IntrinsicElements["img"] >
