98db8a5e266c8ef76c4dbc19f062a683b3d69169
me <irfankf@gmail.com>
2024-10-19 12:59:15 +0700

    after <u> (UNFINISHED!) CHANGES [`packages/studk-ui-iavcodecs/src`] these chgs </u>

--- packages/studk-ui-iavcodecs/src/StudkFfConvDemo.tsx
+++ packages/studk-ui-iavcodecs/src/StudkFfConvDemo.tsx
@@ -99,6 +99,11 @@ import { FFmpeg } from '@ffmpeg/ffmpeg';
 
 
 
+import {
+  useFfAvConvGpv ,
+  useFfAvConvGpvExt ,
+} from "studk-ui-iavcodecs/src/StudkFfConvGpv.tsx";
+
 const StudkFfConvDemoC = (
   StudkReactJs.describeHtmlComponent((
 
@@ -155,149 +160,14 @@ const StudkFfConvDemoInnerC = (
       function (...[coreEngn] : [coreEngn: FFmpeg] )
       {
 
-        const [s0, setS] = (
-          React.useState<{ taskId: number, whenResultAsUrl: Promise<string>, loggedFileRef: React.MutableRefObject<string>, } | null>(null )
-        ) ;
-
-        StudkReactJsEventTargets1.useEventEmitterListener(coreEngn, ["log", ({ message } ) => {
-          if (s0) {
-            const lr = s0.loggedFileRef ;
-            lr.current = lr.current.replace(/\r?\n$/, () => "\r\n") ;
-            lr.current += message.replace(/\r?\n$/, () => "\r\n") ;
-          }
-        }] , [
-          coreEngn,
-          s0 ,
-        ] , {
-          //
-        } ) ;
-
-        const logMsg1 = (
-          StudkReactJsOvcUtil.useIntervalScan(() => (s0?.loggedFileRef.current || "" ) , {
-            latencyMillis: 0.87 * 1000 ,
-            getFallbackValue: () => null ,
-          } )
-        ) ;
-
-        const [s, ] = (
-          (
-            StudkReactJs.useAsyncInterlacing<(
-              | { readonly done: true, readonly previewVideoUrl: string, }
-              | { readonly done: false, readonly error ?: never, }
-              | { readonly done: false, readonly error : Error, }
-              | 0
-            )>(async function* () {
-              C : {
-                ;
-                if (s0) {} else { break C ; }
-                {
-                  try {
-                    ;
-                    yield { done: false , } ;
-                    ;
-                    const url = (
-                      await (
-                        s0.whenResultAsUrl
-                        .catch(z => {
-                          if (z instanceof Error ) {
-                            throw z ;
-                          } else {
-                            throw new TypeError(`Fatal Error ; Exception which is not an Error occured: ${String(z) }`) ;
-                          }
-                        })
-                        .catch(z => (
-                          1 ? throwException(z) :
-                          throwNoResultBcuzExceptionOccuredException(z)
-                        ))
-                      )
-                    ) ;
-                    console["log"]({ url, }) ;
-                    return { previewVideoUrl: url, done: true, } ;
-                  } catch (z) {
-                    return {
-                      done: false,
-                      error: (z instanceof Error ? z : util.throwTypeError() ) ,
-                    } ;
-                  }
-                }
-              }
-              return 0 ;
-            } , [s0] , {
-              onResolverError: e => (
-                console["error"](String(e) )
-              ) ,
-            } )
-            ??
-            { done: false, }
-          )
-          ||
-          false
-        ) ;
-        const gpvHasStarted = !!s ;
-
-        const { fmt, } = (() : { fmt: "mp4" | "webm" } => ({ fmt: "webm" }) )() ;
-
-        const resetToLavfiRendering = (
-          React.useCallback((...[mainCode = `testsrc2=duration=5.1` ]: [code: string] | [code ?: never]) => {
-            const id1 = (
-              util.L.random(20000, 1E6, false )
-            ) ;
-            const r0 = (
-              (async () => {
-                console["log"](`(taskid=${id1 }) beginning` ) ;
-                await (
-                  s0
-                  &&
-                  s0.whenResultAsUrl
-                  .catch(e => console["info"](`(taskid=${id1 }) prior task (id=???) failed with:` , e ) )
-                ) ;
-                console["log"](`(taskid=${id1 }) starting core-pipeline run immediately` ) ;
-                const fnm = "main" + ("." + fmt) ;
-                await coreEngn.exec(["-loglevel", "verbose", "--help"] as const, 50 * 1000 , { } ) ;
-                await (
-                  coreEngn.exec([
-                    "-loglevel", "verbose",
-                    "-f", "lavfi", "-i", mainCode ,
-                    ...([
-                      //
-                      /**
-                       * if we insist on MKV/WEBM
-                       * we shall avoid the autodetection because it'd crash for unidentified reasons
-                       * (this doesn't happen for MP4)
-                       * 
-                       * see https://github.com/ffmpegwasm/ffmpeg.wasm/issues/679
-                       * 
-                       */
-                      "-c:v", "libvpx",
-                      "-c:a", "libvorbis",
-                      fnm ,
-                    ] as const ) ,
-                  ] as const, 50 * 1000 , { } )
-                ) ;
-                console["log"](`(taskid=${id1 }) successful conv` ) ;
-                const fO = await coreEngn.readFile(fnm, ) ;
-                console["log"](`(taskid=${id1 }) successful readback` ) ;
-                if (typeof fO === "string") { return util.throwTypeError() ; }
-                const fOAsUrl = (
-                  toBlockDataUrl({
-                    contentType: "video/" + fmt ,
-                    dataInBytes: fO ,
-                  })
-                ) ;
-                console["log"](`(taskid=${id1 }) successful` ) ;
-                // TODO
-                // setS(() => ({ whenResultAsUrl: Promise , })) ;
-                return util.asConst({ fOAsUrl }) ;
-              })()
-            ) ;
-            setS(() => ({
-              taskId: id1,
-              loggedFileRef: { current: "", } as React.MutableRefObject<string> ,
-              whenResultAsUrl: r0.then(e => e.fOAsUrl ) ,
-            })) ;
-          } , [
-            coreEngn ,
-          ] )
+        const {
+          logMsg1 ,
+          gpvHasStarted ,
+          errorsInfo,
+          previewE ,
+          resetToLavfiRendering ,
+        } = (
+          useFfAvConvGpvExt(coreEngn)
         ) ;
 
         const regen = (
@@ -312,32 +182,13 @@ const StudkFfConvDemoInnerC = (
           //
           logMsg1,
 
-          previewE: (
-            <video
-            {...(
-              s ?
-              (
-                s.done ?
-                { title: `preview`, src: s.previewVideoUrl , controls: true, }
-                : { title: `preview (${!!s.error ? `failed` : `still loading` })`, controls: true, }
-              )
-              :
-              { title: `preview (none yet)`, controls: false, }
-            )}
-            loop
-            style={{
-              /* we deliberately */
-              minWidth   : `20ex`,
-              minHeight  : `10ex`,
-            }}
-            />
-          ) ,
+          previewE ,
 
           regen ,
 
           gpvHasStarted ,
 
-          error: (s && (!s.done && (s.error && String(s.error) ) ) ) || null ,
+          ...errorsInfo ,
         } as const ;
       }
     ) ;
@@ -357,7 +208,7 @@ const StudkFfConvDemoInnerC = (
             previewE ,
             regen ,
             gpvHasStarted ,
-            error,
+            errorMsg: error,
           } = (
             useSpclRec(res)
           ) ;
