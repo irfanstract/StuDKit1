7f144097a6862679cebb5442b6aca701a47e78dc
me <irfankf@gmail.com>
2024-12-06 13:56:53 +0700

    after __partially__ reverting earlier commit f354d6f20d04da05a7b5ba7a04ff38239a9df145 (it says: Merge branch 'StudiokitTsNodeNodeRequireAndLoadersResolvers20241204a151' )

--- packages/stdkjs/packages/@studiokit/ts-node/dist-raw/node-internal-modules-package_json_reader.js
+++ packages/stdkjs/packages/@studiokit/ts-node/dist-raw/node-internal-modules-package_json_reader.js
@@ -1,6 +1,4 @@
 // copied from https://github.com/nodejs/node/blob/v15.3.0/lib/internal/modules/package_json_reader.js
-
-// @ts-check
 'use strict';
 
 const { SafeMap } = require('./node-primordials');
@@ -11,12 +9,11 @@ const { toNamespacedPath } = require('path');
 
 const cache = new SafeMap();
 
-/** @type {any} */
 let manifest;
 
 /**
  * @param {string} jsonPath
- * @return {import('@studiokit/ts-node').LoadedNodePackageConfig}
+ * @return {{string: string, containsKeys: boolean}}
  */
 function read(jsonPath) {
   if (cache.has(jsonPath)) {
