2df8d7282cc042abc2abbc702e31ccd38d59ff56
me <irfankf@gmail.com>
2024-12-14 11:28:01 +0700

    after [Code] [`packages/stdkjs/packages/@studiokit/ts-node`] [`tests/applied-dev-server`] these WIP(s)

---
+++ packages/stdkjs/packages/@studiokit/ts-node/tests/applied-dev-server/sites/SummerHitsPlayMeApp/pages/MyIconJpg.tsx
@@ -0,0 +1,41 @@
+
+
+
+
+
+import React = require("react");
+
+
+
+
+
+
+export = (
+  <div>
+    <p>
+      <a href="myicon.png" children={<code>myicon.png</code>} />
+    </p>
+    <figure>
+      <img
+      src="myicon.png"
+      />
+      <figcaption
+      children={(
+        <p>
+          here <code>myicon.png</code> y'all
+        </p>
+      )}
+      />
+    </figure>
+  </div>
+) ;
+
+
+
+
+
+
+
+
+
+
