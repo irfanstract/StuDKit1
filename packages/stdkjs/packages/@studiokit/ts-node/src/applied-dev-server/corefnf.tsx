c608cc407bd860da457d4f65f5b817782fbbf53c
me <irfankf@gmail.com>
2024-12-22 11:49:35 +0700

    after [Code] [`packages/stdkjs/packages/@studiokit/ts-node`] these chgs around `applied-dev-server` (specifically chgs to `appfmt*` and/or related files )

---
+++ packages/stdkjs/packages/@studiokit/ts-node/src/applied-dev-server/corefnf.tsx
@@ -0,0 +1,133 @@
+
+
+
+
+// /* @jsxRuntime classic */
+// /* @jsx classic */
+
+
+
+
+// /// <reference lib="ES2022" />
+/// <reference lib="DOM" />
+
+
+
+
+
+
+import {
+  getStackOrMessage,
+  hasOwnProperty,
+  memoize,
+  assert ,
+  once,
+  parse,
+  ProjectLocalResolveHelper,
+  utilReiterated,
+  split,
+  resolveUrl ,
+  versionGteLt,
+  yn,
+  type ArgsWithOptions, 
+  Immutable,
+  isUnderCspNoEvalsPolicy,
+  dropSearchParamAndHash,
+  mutationallyTransformUrl,
+} from '../util';
+
+import {
+  once1 ,
+  L ,
+  inspect ,
+  parseUrl ,
+  Dispatch ,
+} from "./util-alt" ;
+
+import type {
+
+  AllOrNeither,
+  ConformOrNever, 
+  EitherOneProp,
+
+  // PartializeOptionsConditionally ,
+  // PartializeOptionsConditionallyAndRequifyIfFalse ,
+
+  // MayOptRecord ,
+  // MayOptRecordRevalue ,
+
+} from "../util-recordtypes" ;
+
+
+
+;
+
+
+
+
+
+
+
+import {
+  React ,
+  ReactDOM ,
+  ReactDOMServer ,
+} from "./util-ws" ;
+
+
+
+
+
+
+
+
+export const CoreFnfC = (
+
+  function CoreFnfCImpl()
+  {
+
+    return (
+      <>
+      <div style={{
+        float: "inline-start",
+        position: "sticky",
+        insetBlockStart: 0,
+        insetInlineStart: 0,
+        blockSize: `100%`,
+        zIndex: 32000 ,
+        backdropFilter: `blur(0.1ex)` ,
+      }}>
+      <p><span style={{ fontSize: `2em`, }} children={`🚫`}/>
+      </p>
+      </div>
+      <div style={{
+        position: "sticky",
+        insetBlockStart: 0,
+        insetInlineStart: 0,
+        zIndex: 2000 ,
+        backdropFilter: `blur(0.1ex)` ,
+      }}>
+      <h1>Not a Public Page</h1>
+      </div>
+      <div>
+        <p>This Page Doesn't Correspond To A Public Page. Access Denied.</p>
+        <p>Make Sure The Path (In The Clicked Link) Is Properly Spelled.</p>
+      </div>
+      </>
+    ) ;
+  }
+) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
