3e3f0e2f5e1d913c64eeb804513581354fb5b767
me <irfankf@gmail.com>
2025-01-03 08:28:48 +0700

    after [Code] [`packages/stdkjs/packages/@studiokit/electron-mocha`] these (partially-bugfixing) fixups while starting working at `@studiokit/electron-mocha` (these chgs involves `@studiokit/electron-mocha` ) (WIP)

--- packages/stdkjs/packages/@studiokit/electron-mocha/lib/run.js
+++ packages/stdkjs/packages/@studiokit/electron-mocha/lib/run.js
@@ -1,3 +1,6 @@
+
+// @ts-check
+
 'use strict'
 
 const { join, resolve } = require('path')
@@ -183,6 +186,8 @@ exports.handler = async argv => {
         }
       })
     } else {
+      console["warn"](`Offloading The Subject Into Renderer Ctx, As Configured`)
+
       const customWindowOptions = argv['window-config']
         ? require(resolve(argv['window-config']))
         : {}
@@ -197,7 +202,7 @@ exports.handler = async argv => {
           contextIsolation: true,
           nodeIntegration: true,
           ...customWindowOptions.webPreferences,
-          preload: join(__dirname, '..', 'renderer', 'run.js')
+          preload: join(__dirname, '..', 'renderer', 'run-with-ts.js')
         }
       })
 
@@ -257,7 +262,7 @@ exports.handler = async argv => {
 const fail = (error, trace) => {
   console.error(`\n${ansi.red('ERROR:')} ${error.message}`)
   if (trace && error.stack) console.error(error.stack)
-  app.exit(1)
+  0 && app.exit(1)
 }
 
 const warn = warning => {
